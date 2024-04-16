6cd5e73364b21fcdd6b54b44e5a0ff8df65cb7e2
Joshua Zhou <joshuazhou0808@gmail.com>
2024-04-15 17:30:22 -0700

    Fix Wide Screen Compatabtility Issues and Minor Hero Design Flaws

--- src/home.js
+++ src/home.js
@@ -8,25 +8,33 @@ function createHomePage(){
     const hero = document.createElement('div');
     hero.style.backgroundImage = `url(${Hero})`;  
     hero.style.height = '600px';
-    hero.style.width = '100vw';
+    hero.style.width = '1600px';
     hero.style.backgroundSize = 'cover'
+    hero.style.backgroundPosition = 'bottom'
 
     heroSection.appendChild(hero)
+
+    const textWrapper = document.createElement('div')
+    textWrapper.classList.add('wrapper')
+
+    hero.appendChild(textWrapper)
 
     const heroText = document.createElement('h1')
     heroText.textContent = "The New Standard For Online Math Tutoring"
 
-    hero.appendChild(heroText)
+    textWrapper.appendChild(heroText)
 
     const heroSubText = document.createElement('p')
     heroSubText.textContent = "Recommended by the world's leading math professors, our tutoring will prepare your child to ace their next math exam. Schedule your first lesson today and together, lets help your child shine."
 
-    hero.appendChild(heroSubText)
+    textWrapper.appendChild(heroSubText)
 
     const startBtn = document.createElement('button')
     startBtn.textContent = "Get Started"
     startBtn.id = "get-started"
 
-    hero.appendChild(startBtn)
+    textWrapper.appendChild(startBtn)
+
+
 }
 export {createHomePage}
