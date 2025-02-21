ae631c115d3e0e430baa0d6d9aa8a613d2044b15
Joshua Zhou <joshua.zhou0808@gmail.com>
2025-02-20 19:02:10 -0800

    Add Amanda and Preston

--- src/JS/testimonials.js
+++ src/JS/testimonials.js
@@ -4,13 +4,15 @@ import "../Styling/nav.css";
 import "../Styling/footer.css";
 import "./nav_footer.js";
 
+const Amanda = "EyNo302xfiY";
+const Preston = "c5kQEMJbbKM";
 const Abby = "VSfYwtCr0x0";
 const Aileen = "nesjwfajFIM";
 const Danny = "OCY7pB0Q4jc";
 const Jacob = "pt0aDFxYEfM";
 const Veer = "CvjY9WDI73w";
 
-const videos = [Abby, Aileen, Danny, Jacob, Veer];
+const videos = [Amanda, Preston, Abby, Aileen, Danny, Jacob, Veer];
 const videoWrapper = document.querySelector(".video-wrapper");
 for (const video of videos) {
   console.log(video);
